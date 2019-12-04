55baa57944b34fb4f8ad95d66c234519cede0df9
Rohit Verma <rohit.verma@niki.ai>
2019-12-04 12:00:31 +0530

    moving to openfeign

--- service/src/main/java/org/apache/fineract/cn/accounting/service/AccountingServiceConfiguration.java
+++ service/src/main/java/org/apache/fineract/cn/accounting/service/AccountingServiceConfiguration.java
@@ -35,6 +35,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
@@ -56,7 +57,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
         CustomerManager.class
     }
 )
-public class AccountingServiceConfiguration extends WebMvcConfigurerAdapter {
+public class AccountingServiceConfiguration implements WebMvcConfigurer {
 
   public AccountingServiceConfiguration() {
     super();
